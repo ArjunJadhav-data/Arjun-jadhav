7a59b896500099ed11b540e5c8ba46ffe608f4e1
Arjun Jadhav <jadhavarjun9545@gmail.com>
2025-12-28 15:00:37 +0530

    first commit

---
+++ S2.py
@@ -0,0 +1,7 @@
+a=input("Enter something: ")
+b =input("Enter something else: ")
+c=a+b
+print("The concatenated result is:"+ c) 
+print(type(a))
+print(type(b))
+print(type(c))
---
+++ first.py
@@ -0,0 +1,4 @@
+int a =34
+int b= 23
+c=a+b
+print("sum of " +c)
